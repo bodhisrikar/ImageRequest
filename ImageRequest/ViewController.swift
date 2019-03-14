d55224dfa18a93be8b318b011dbb39ca7242e99d
Srikar Thottempudi <srikarthottempudi@srikars-macbook-air.local>
2019-03-13 22:54:13 -0400

    Initial Commit

---
+++ ImageRequest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageRequest
+//
+//  Created by Srikar Thottempudi on 3/13/19.
+//  Copyright © 2019 Srikar Thottempudi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
